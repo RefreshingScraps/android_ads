62421ac29553d10adaf3f05b010a171f624959d8
RefreshingScraps <hzy2648@outlook.com>
2026-03-20 17:58:57 +0800

    新增开屏页

---
+++ app/src/main/kotlin/com/example/ads/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.ads
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_splash)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+}
